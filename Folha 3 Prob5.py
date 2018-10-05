0550d14fb9546947db65657418bd289754a0b430
Pedro <43143831+PedroLuisPT@users.noreply.github.com>
2018-10-05 18:46:04 +0100

    Update Folha 3 Prob5.py

--- Folha 3 Prob5.py
+++ Folha 3 Prob5.py
@@ -1,4 +1,4 @@
-The idea is to write a number and a word in order to relate the number 0 to first letter, 1 to second and so on.
+#The idea is to write a number and a word in order to relate the number 0 to first letter, 1 to second and so on.
 i=0
 k=int(input("Write a number"))
 w=input("write a word")
