49497b6924fc7aea8874b474510e347554e29fbb
Pedro <43143831+PedroLuisPT@users.noreply.github.com>
2018-10-07 14:58:08 +0100

    Update Division.py

--- Division.py
+++ Division.py
@@ -1,8 +1,8 @@
 #This code asks the user for a Dividend and a Divider, returning the quotient and solution's rest
 dividend = int(input("Dividend: "))
 divider = int(input("Divider: "))
-rest = dividend
-quotient = 0
+rest = dividend % divider
+quotient = dividend//divider
 while rest >= divider:
  rest = rest - divider
  quotient = quotient + 1
