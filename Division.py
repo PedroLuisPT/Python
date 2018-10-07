832b6c9f19a4297e5cc5b13d9bf1538c42b3c6ca
Pedro <43143831+PedroLuisPT@users.noreply.github.com>
2018-10-07 14:47:23 +0100

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,8 @@
+dividend = int(input("Dividend: "))
+divider = int(input("Divider: "))
+rest = dividend
+quotient = 0
+while rest >= divider:
+ rest = rest - divider
+ quotient = quotient + 1
+print("The quotient is", quotient, "and the rest is", rest)
