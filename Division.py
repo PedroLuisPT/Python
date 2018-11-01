b33ecead6d8e0a14ee3707ab5346b522dad7a489
Pedro <43143831+PedroLuisPT@users.noreply.github.com>
2018-11-01 16:23:34 +0000

    Update Division.py

--- Division.py
+++ Division.py
@@ -1,10 +1,16 @@
-#This code asks the user for a Dividend and a Divider, returning the quotient and solution's rest
-dividend = int(input("Dividend: "))
-divider = int(input("Divider: "))
-rest = dividend % divider
-quotient = dividend//divider
-while rest >= divider:
- rest = rest - divider
- quotient = quotient + 1
-print("The quotient is", quotient, "and the rest is", rest)
-
+def division(x,y)
+  """
+  This code asks the user for a Dividend and a Divider, returning the quotient and solution's rest
+  Requires:Two int numbers y>0
+  Ensures: The quotient and the rest of the division
+  """
+  dividend = int(input("Dividend: "))
+  divider = int(input("Divider: "))
+  rest = dividend % divider
+  quotient = dividend//divider
+  while rest >= divider:
+    rest = rest - divider
+    quotient = quotient + 1
+    print("The quotient is", quotient, "and the rest is", rest)
+ return quotient
+ return rest
