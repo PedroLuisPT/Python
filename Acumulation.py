3972b0b6d8b2071ca413def1565360c1b576e97b
Pedro <43143831+PedroLuisPT@users.noreply.github.com>
2018-11-01 16:20:00 +0000

    Update Acumulation.py

--- Acumulation.py
+++ Acumulation.py
@@ -1,12 +1,12 @@
 def sumCicles()
-"""
-Write a code which reads a int number "n" and writes the sum of the int numbers between 0 e n. Using the cicle "for".
-Requires: a int number >0
-Ensures: the sum of all the int numbers between 0 and the given nunber
-"""
-n=int(input("Write a number"))
-acum=0
-for i in range(n):
-    acum+=i
-    print(acum)
-return acum    
+    """
+    Write a code which reads a int number "n" and writes the sum of the int numbers between 0 e n. Using the cicle "for".
+    Requires: a int number >0
+    Ensures: the sum of all the int numbers between 0 and the given nunber
+    """
+    n=int(input("Write a number"))
+    acum=0
+    for i in range(n):
+        acum+=i
+        print(acum)
+    return acum    
