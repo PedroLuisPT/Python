ea2d58d1bf0a223c113a0778f1b8afa467479b5c
Pedro <43143831+PedroLuisPT@users.noreply.github.com>
2018-11-01 16:32:59 +0000

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,16 +1,15 @@
 def password()
-			"""			
-			This is a simple guess the password example program counting the tryouts of guessing the right word
-			Requires: Guessing word (str)
-			Ensures: Congratulation Advice when guessed
-			"""
-
-			p = "IlovePython"
-			x = ""
-			count=0
-			while x != p:
- 						x = input("Password: ")
- 						print(count)
- 						count=count+1
-								print ("Congratulations, the password is right!")
-			return count
+	"""			
+	This is a simple guess the password example program counting the tryouts of guessing the right word
+	Requires: Guessing word (str)
+	Ensures: Congratulation Advice when guessed
+	"""
+	p = "IlovePython"
+	x = ""
+	count=0
+	while x != p:
+		x = input("Password: ")
+		print(count)
+		count=count+1
+		print ("Congratulations, the password is right!")
+	return count
